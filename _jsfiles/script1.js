1c648bbb5072442214150ca5aa4c191fbf3edb78
Joshua Johnson <joshuaj808@gmail.com>
2016-01-18 16:25:55 -0500

    js working on index, some basic css in place, design next

---
+++ _jsfiles/script1.js
@@ -0,0 +1,26 @@
+
+// $(document).ready(
+// 	function(){
+// 		alert("hi there mofo");
+// 	}
+// 	);
+
+
+
+$("#surprise").hide();
+
+
+function magicTime(){
+	$("#surprise").show();
+};
+
+
+setTimeout(magicTime, 2000);
+// magicTime();
+
+
+$("#exit").click(
+	function(){
+		$("#surprise").hide();
+	}
+);
