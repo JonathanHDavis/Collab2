761e0c400c87b43847c65b19b9a6528808778b8b
Jonathan Davis <jonathanherscheldavis@gmail.com>
2016-01-18 22:20:13 -0500

    styled producks

--- _jsfiles/Producks.js
+++ _jsfiles/Producks.js
@@ -1,80 +1,80 @@
 $('#Duck1').hover(
 	function() {
-		$('.test1').removeClass('hidden1');
+		$('.test1').removeClass('.hidden1');
 	}, 
 	function () {
-		$('.test1').addClass('hidden1');
+		$('.test1').addClass('.hidden1');
 	}
 	);
 
 $('#Duck2').hover(
 	function() {
-		$('.test2').removeClass('hidden2');
+		$('.test2').removeClass('.hidden2');
 	}, 
 	function () {
-		$('.test2').addClass('hidden2');
+		$('.test2').addClass('.hidden2');
 	}
 	);
 
 $('#Duck3').hover(
 	function() {
-		$('.test3').removeClass('hidden3');
+		$('.test3').removeClass('.hidden3');
 	}, 
 	function () {
-		$('.test3').addClass('hidden3');
+		$('.test3').addClass('.hidden3');
 	}
 	);
 
 $('#Duck4').hover(
 	function() {
-		$('.test4').removeClass('hidden4');
+		$('.test4').removeClass('.hidden4');
 	}, 
 	function () {
-		$('.test4').addClass('hidden4');
+		$('.test4').addClass('.hidden4');
 	}
 	);
 
 $('#Duck5').hover(
 	function() {
-		$('.test5').removeClass('hidden5');
+		$('.test5').removeClass('.hidden5');
 	}, 
 	function () {
-		$('.test5').addClass('hidden5');
+		$('.test5').addClass('.hidden5');
 	}
 	);
 
 $('#Duck6').hover(
 	function() {
-		$('.test6').removeClass('hidden6');
+		$('.test6').removeClass('.hidden6');
 	}, 
 	function () {
-		$('.test6').addClass('hidden6');
+		$('.test6').addClass('.hidden6');
 	}
 	);
 
 $('#Duck7').hover(
 	function() {
-		$('.test7').removeClass('hidden7');
+		$('.test7').removeClass('.hidden7');
 	}, 
 	function () {
-		$('.test7').addClass('hidden7');
+		$('.test7').addClass('.hidden7');
 	}
 	);
 
 $('#Duck8').hover(
 	function() {
-		$('.test8').removeClass('hidden8');
+		$('.test8').removeClass('.hidden8');
 	}, 
 	function () {
-		$('.test8').addClass('hidden8');
+		$('.test8').addClass('.hidden8');
 	}
 	);
 
 $('#Duck9').hover(
 	function() {
-		$('.test9').removeClass('hidden9');
+		$('.test9').removeClass('.hidden9');
 	}, 
 	function () {
-		$('.test9').addClass('hidden9');
+		$('.test9').addClass('.hidden9');
 	}
 	);
